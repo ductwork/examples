87633cab15ed82a5f201b10181efb7ed9fc2de11
Tyler Ewing <tewing10@gmail.com>
2025-11-18 08:42:58 -0600

    feat: add README instrutions for enrich user data pipeline

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Rails application with example pipelines
 ## Setup
 
 Run `bin/setup`
+
+To re-seed the database run `bin/setup --reset`
+
+## Usage
+
+This application includes a number of example pipelines meant to get you familiar with them and inspire you to design your own. You can view progress in the logs or on the console.
+
+### `EnrichAllUsersDataPipeline`
+
+This pipeline is meant to minic a typical data enrichment process where data is fetched from multiple sources in parallel and then merged back into the record and persisted in the database.
+
+You can trigger the user enrichment pipeline on the rails console with `EnrichAllUsersDataPipeline.trigger(7)` or with the rake task `bin/rails enrich_user_data`.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+1_000.times do |i|
+  User.create!(
+    name: Faker::Name.name,
+    age: Kernel.rand(21..99),
+    favorite_food: Faker::Food.dish,
+    last_enriched_at: 10.days.ago
+  )
+  puts "Created user ##{i + 1}"
+end
